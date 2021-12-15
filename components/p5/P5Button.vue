c529de63746312b8cac3969ea30892ecce0cd5dc
Jumanji81 <deenl@gmx.com>
2021-12-15 20:45:48 +0100

    Added filler to about component for eslint check, excluded node-modules from jsconfig.

---
+++ components/p5/P5Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>About</h1>
+    <!-- <div id="p5Button" /> -->
+</template>
+
+<script setup>
+import {onMounted} from '@vue/runtime-core';
+
+onMounted(async () => {
+    // const {p5Button} = await import('../../p5');
+    // .then(d5 => {
+    //     console.log(d5);
+    // })
+    // .catch(() => {
+    //     console.log('catch');
+    // });
+});
+</script>
+
+<style scoped>
+#p5Button {
+    /* position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%); */
+    height: 100px;
+}
+</style>
