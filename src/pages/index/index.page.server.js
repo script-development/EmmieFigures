943fd28ae17a0f0122663b4cd6283c3401a8c9be
EvenhuisD <devenhuis81@gmail.com>
2022-03-10 13:16:54 +0100

    getting reports from 1st jan 2021 to now

--- src/pages/index/index.page.server.js
+++ src/pages/index/index.page.server.js
@@ -1,12 +1,16 @@
 import {getFromApi} from 'services/api';
+import {yesterdayQueryString} from 'services/dates';
 
 export {onBeforeRender};
 
 /** @param {import("types").PageContext} context */
 async function onBeforeRender(context) {
     let reportData;
+    let startDate = '2021-01-01';
+    let endDate = yesterdayQueryString();
+
     try {
-        reportData = await getFromApi('http://127.0.0.1:8000/api/reports-presence/2021-01-01/2021-12-31');
+        reportData = await getFromApi(`http://127.0.0.1:8000/api/reports-presence/${startDate}/${endDate}`);
     } catch (error) {
         console.error(error); // eslint-disable-line no-console
     }
