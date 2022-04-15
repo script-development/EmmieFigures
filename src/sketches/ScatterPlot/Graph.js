2d017b89441aa26ca80bb3819e328019964e0a38
EvenhuisD <devenhuis81@gmail.com>
2022-04-15 15:37:13 +0200

    Moar refactorz

--- src/sketches/ScatterPlot/Graph.js
+++ src/sketches/ScatterPlot/Graph.js
@@ -1,8 +1,5 @@
 /** @typedef {import('types/graph').GraphData} GraphData */
-// /** @typedef {import('types/graph').Stat} Stat */
-
-/** @type {CanvasRenderingContext2D} */
-let ctx;
+import Paint from '../paint';
 
 /** @type {import("types/sketches").Sketch["grid"]["properties"]} */
 let grid;
@@ -23,7 +20,7 @@ const defaults = {
 };
 
 /** @type {Object.<string, any>} */
-const elements = {
+export const elements = {
     x: {
         pos: {x1: 0, y1: 0, x2: 0, y2: 0},
         color: 'black',
@@ -39,17 +36,34 @@ const elements = {
     xTitle: {
         ...defaults.text,
         pos: {x: 0, y: 0},
+        kind: '',
     },
     yTitle: {
         ...defaults.text,
         pos: {x: 0, y: 0},
+        kind: '',
         angle: -Math.PI / 2,
     },
     mainTitle: {
         ...defaults.text,
+        text: 'Scatterplot',
         pos: {x: 0, y: 0},
-        weight: 32,
-        normal: 'bold',
+        size: 32,
+        weight: 'bold',
+    },
+    xUnits: {
+        units: [],
+        maxValue: 0,
+        minValue: 0,
+        length: 0,
+        startX: 0,
+    },
+    yUnits: {
+        units: [],
+        maxValue: 0,
+        minValue: 0,
+        length: 0,
+        startY: 0,
     },
 };
 
@@ -80,7 +94,7 @@ const setPositions = () => {
     };
     elements.xTitle.pos = {x: grid.unitWidth * 16, y: grid.unitHeight * 16};
     elements.yTitle.pos = {x: grid.unitWidth * 2, y: grid.unitHeight * 9};
-    elements.mainTitle.pos = {x: grid.width * 0.5, y: grid.unitHeight * 3};
+    elements.mainTitle.pos = {x: grid.width * 0.5, y: grid.unitHeight * 2};
 };
 
 /**
@@ -88,15 +102,16 @@ const setPositions = () => {
  * @param {import("types/sketches").Sketch} sketch
  * @returns
  */
-export default sketch => {
-    ctx = sketch.context;
+export const Graph = sketch => {
     grid = sketch.grid.properties;
+    const paint = Paint(sketch.context);
     setPositions();
+    // careful: this might not be a good place for it with future alterations
 
     const show = () => {
         Object.keys(elements).map(key => {
             if (key == 'xUnits' || key == 'yUnits')
-                elements[key].map((/** @type {{ show: string }} */ el) => paint[el.show](el));
+                elements[key].units.map((/** @type {{ show: string }} */ el) => paint[el.show](el));
             else paint[elements[key].show](elements[key]);
         });
     };
@@ -132,13 +147,23 @@ const units = (x1, y1, x2, y2, steps, data) => {
             ),
         );
     }
-    return units;
+    return {
+        units,
+        min: minRounded,
+        max: maxRounded,
+        lengthX: x2 - x1,
+        lengthY: y2 - y1,
+        x1,
+        y1,
+    };
 };
 
 /** @param {GraphData} dataX */
-export const setX = dataX => {
+export const setGraphX = dataX => {
     elements.xTitle.text = `${dataX.title} (${dataX.unitOfMeasure})`;
-    elements['xUnits'] = units(
+    elements.xTitle.kind = dataX.title.toLowerCase();
+    elements.mainTitle.text = `Scatterplot voor ${elements.yTitle.kind} vs ${elements.xTitle.kind}`;
+    const xUnits = units(
         elements.x.pos.x1 + grid.unitWidth * 0.25,
         elements.x.pos.y1 + grid.unitHeight * 0.5,
         elements.x.pos.x2 - grid.unitWidth * 0.25,
@@ -146,12 +171,19 @@ export const setX = dataX => {
         2,
         dataX.data,
     );
+    elements.xUnits.units = xUnits.units;
+    elements.xUnits.maxValue = xUnits.max;
+    elements.xUnits.minValue = xUnits.min;
+    elements.xUnits.length = xUnits.lengthX;
+    elements.xUnits.startX = xUnits.x1;
 };
 
 /** @param {GraphData} dataY */
-export const setY = dataY => {
+export const setGraphY = dataY => {
     elements.yTitle.text = `${dataY.title} (${dataY.unitOfMeasure})`;
-    elements['yUnits'] = units(
+    elements.yTitle.kind = dataY.title.toLowerCase();
+    elements.mainTitle.text = `Scatterplot voor ${elements.yTitle.kind} vs ${elements.xTitle.kind}`;
+    const yUnits = units(
         elements.y.pos.x1 - grid.unitWidth * 0.5,
         elements.y.pos.y1 - grid.unitHeight * 0.25,
         elements.y.pos.x2 - grid.unitWidth * 0.5,
@@ -159,63 +191,9 @@ export const setY = dataY => {
         10,
         dataY.data,
     );
+    elements.yUnits.units = yUnits.units;
+    elements.yUnits.maxValue = yUnits.max;
+    elements.yUnits.minValue = yUnits.min;
+    elements.yUnits.length = yUnits.lengthY;
+    elements.yUnits.startY = yUnits.y1;
 };
-
-/** @type {Object.<string, any>} */
-const paint = {
-    /** @param {defaults["text"] & {pos: {x: number, y: number}, angle?: number}} param */
-    text: ({color, align, baseline, weight, size, font, text, pos, angle}) => {
-        ctx.fillStyle = color;
-        ctx.textAlign = align;
-        ctx.textBaseline = baseline;
-        ctx.font = `${weight} ${size}px ${font}`;
-        if (angle) {
-            ctx.save();
-            ctx.translate(pos.x, pos.y);
-            ctx.rotate(angle);
-            ctx.fillText(text, 0, 0);
-            ctx.restore();
-        } else ctx.fillText(text, pos.x, pos.y);
-    },
-    /** @param {elements['x'] | elements['y']} element */
-    line: element => {
-        ctx.strokeStyle = element.color;
-        ctx.lineWidth = element.weight;
-        ctx.beginPath();
-        ctx.moveTo(element.pos.x1, element.pos.y1);
-        ctx.lineTo(element.pos.x2, element.pos.y2);
-        ctx.stroke();
-    },
-};
-
-// /** @param {GraphData["data"]} typeY */
-// const yAxisUnits = typeY => {
-//     const pos = {x1: origin.x, y1: origin.y, x2: origin.x, y2: ctx.canvas.height * 0.2};
-//     const maxValue = typeY.reduce((a, {value}) => Math.max(a, value), 0);
-//     const minValue = 0;
-//     const steps = 10;
-//     const unitSY = pos.y1 - 10;
-//     const unitEY = pos.y2 + 10;
-//     const length = unitEY - unitSY;
-//     const show = () => {
-//         ctx.fillStyle = 'black';
-//         for (let j = 0; j <= steps; j++) {
-//             let yPos = unitSY + (length / steps) * j;
-//             ctx.lineWidth = 2;
-//             ctx.strokeStyle = 'black';
-//             ctx.beginPath();
-//             ctx.moveTo(pos.x1 - 5, yPos);
-//             ctx.lineTo(pos.x1 + 5, yPos);
-//             ctx.stroke();
-//             ctx.lineWidth = 1;
-//             ctx.strokeStyle = '#ddd';
-//             ctx.moveTo(pos.x1, yPos);
-//             ctx.lineTo(pos.x1 + ctx.canvas.width * 0.6, yPos);
-//             ctx.stroke();
-//             ctx.textBaseline = 'middle';
-//             ctx.font = '16px georgia';
-//             ctx.fillText((minValue + ((maxValue - minValue) / steps) * j).toFixed(2), pos.x1 - 30, yPos);
-//         }
-//     };
-//     return {show, length, unitSY, minValue, maxValue};
-// };
